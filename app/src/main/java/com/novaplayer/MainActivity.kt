8829de731ade03b3b49fefce0f344fab19db1509
Sp00kyTec <addictionunique@gmail.com>
2025-09-14 19:10:43 +0300

    base Android project

---
+++ app/src/main/java/com/novaplayer/MainActivity.kt
@@ -0,0 +1,12 @@
+// app/src/main/java/com/novaplayer/MainActivity.kt
+package com.novaplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
